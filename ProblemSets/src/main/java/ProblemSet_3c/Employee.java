451fe134f6526808450b4b7c3b96b7bfbb023333
Gayle King <lk00814@surrey.ac.uk>
2023-10-03 11:46:01 +0100

    Updating Files.

---
+++ ProblemSets/src/main/java/ProblemSet_3c/Employee.java
@@ -0,0 +1,55 @@
+package ProblemSet_3c;
+public class Employee {
+
+	int id;
+	String forename;
+	String surname;
+	AnnualSalary salary;
+	Position Position;
+	
+	public Employee(int id, String forename, String surname, AnnualSalary salary, Position Position) {
+		this.forename = forename;
+		this.surname = surname;
+		this.salary = salary;
+		this.Position = Position;
+		this.id = id;
+	}
+	public String getForename() {
+		return forename;
+	}
+	
+	public String getSurname() {
+		return surname;
+	}
+	
+	public double getSalary() {
+		return salary.getSalary();
+	}
+
+	public String getPositionName() {
+		return Position.getRoleName();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void displayEmployeeName() {
+	 System.out.println(forename + " " + surname);
+	}
+
+	public boolean eligibleForBonus() {
+		if (salary.getSalary() >= 40000) {
+			return true;
+		}
+		return false;
+	}
+
+	public String toString() {
+		if (eligibleForBonus() == true) {
+		return (surname + ", " + forename + " (" + id + "): " + Position.getRoleName() + " at �" + salary.getSalary() + " (�" + salary.calculateTax() +" tax) and is eligible for bonus.");
+	}
+		return (surname + ", " + forename + " (" + id + "): " + Position.getRoleName() + " at �" + salary.getSalary() + " (�" + salary.calculateTax() +" tax) and is not eligible for bonus.");
+
+	}
+}
---
+++ ProblemSets/src/main/java/ProblemSet_3c/Position.java
@@ -0,0 +1,17 @@
+package ProblemSet_3c;
+
+public class Position {
+	
+	public Position() {
+		
+	}
+	String RoleName;
+	
+	public String getRoleName() {
+		return RoleName;
+	}
+	
+	public void setRoleName(String role) {
+		RoleName = role;
+	}
+}
