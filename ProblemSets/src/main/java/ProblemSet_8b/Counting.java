b99927cd408c11877953153fe929b86a13b04a3e
Gayle King <lk00814@surrey.ac.uk>
2023-10-30 18:27:58 +0000

    ProblemSet_8b

--- ProblemSets/src/main/java/ProblemSet_8b/Counting.java
+++ ProblemSets/src/main/java/ProblemSet_8b/Counting.java
@@ -1,19 +1,32 @@
 package ProblemSet_8b;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Counting {
 	public Counting() {
-		
+
 	}
-	
+
 	public String readTextFile(String file) {
 		File myFile = new File(file);
-		Scanner read = new Scanner(myFile);
+		try {
+			Scanner read = new Scanner(myFile);
+			StringBuffer output = new StringBuffer();
+			int counter = 1;
+			while (read.hasNextLine()) {
+				output.append(displayLine(read.nextLine(), counter));
+				counter++;
+			}
+			read.close();
+			return output.toString();
+		} catch (FileNotFoundException e) {
+			return file;
+		}
 	}
-	
+
 	private String displayLine(String line, int counter) {
-		
+		return (line + " : " + counter + "\n");
 	}
 }
